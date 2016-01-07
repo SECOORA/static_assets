7e1d3801a8c0d2c0b6a3fc95e0873db55dc31c65
ocefpaf <ocefpaf@gmail.com>
2016-01-07 12:40:22 -0300

    Glider Track and Model Grid

---
+++ glider_tracks/convert.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# conda execute
+# env:
+#  - fiona
+# channels:
+#  - ioos
+# run_with: python
+
+"""
+Create GeoJSON Features for SECOORA Glider Tracks.
+
+To test the features copy-and-paste the GeoJSON file onto:
+
+http://geojson.io/
+
+Run with,
+$ conda execute -v convert.py
+
+"""
+
+import json
+import fiona
+
+import logging
+logger = logging.getLogger()
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.WARNING)
+
+
+def main():
+    features = []
+    with fiona.open('georef_glider_triangles.shp') as shp:
+        for k, item in shp.items():
+            features.append(item)
+
+    fc = {'features': features, 'type': 'FeatureCollection'}
+    with open('georef_glider_triangles.geojson', 'w') as f:
+        f.write(json.dumps(fc, sort_keys=True,
+                           indent=2, separators=(",", ": ")))
+
+if __name__ == "__main__":
+    main()
